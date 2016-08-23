bb2408226ff189792c1ab718bdb6ae0bfe991629
Diba Mirza <dimirza@eng.ucsd.edu>
2016-08-22 20:51:30 -0700

    Testing the motor functions

--- motorTest.py
+++ motorTest.py
@@ -1,108 +1,30 @@
-# Pizazz Motor Test
+ Pizazz Motor Test
 # Moves: Forward, Reverse, turn Right, turn Left, Stop - then repeat
 # Press Ctrl-C to stop
-#
-# Also demonstrates writing to the LEDs
 #
 # To check wiring is correct ensure the order of movement as above is correct
 # Run using: sudo python motorTest.py
 
 
-import RPi.GPIO as GPIO, sys, threading, time
+import RPi.GPIO as GPIO, time
 
+from motor import *
 #use physical pin numbering
 GPIO.setmode(GPIO.BOARD)
-
-#use pwm on inputs so motors don't go too fast
-# Pins 19, 21 Right Motor
-# Pins 24, 26 Left Motor
-GPIO.setup(19, GPIO.OUT)
-p=GPIO.PWM(19, 20)
-p.start(0)
-GPIO.setup(21, GPIO.OUT)
-q=GPIO.PWM(21, 20)
-q.start(0)
-GPIO.setup(24, GPIO.OUT)
-a=GPIO.PWM(24,20)
-a.start(0)
-GPIO.setup(26, GPIO.OUT)
-b=GPIO.PWM(26,20)
-b.start(0)
-
-slowspeed = 20
-fastspeed = 100
-LED1 = 22
-LED2 = 18
-LED3 = 11
-LED4 = 07
-GPIO.setup(LED1, GPIO.OUT)
-GPIO.setup(LED2, GPIO.OUT)
-GPIO.setup(LED3, GPIO.OUT)
-GPIO.setup(LED4, GPIO.OUT)
-
-def forwards():
-  p.ChangeDutyCycle(fastspeed)
-  q.ChangeDutyCycle(0)
-  a.ChangeDutyCycle(fastspeed)
-  b.ChangeDutyCycle(0)
-  setLEDs(1, 0, 0, 1)
-  print('straight')
-
-def reverse():
-  p.ChangeDutyCycle(0)
-  q.ChangeDutyCycle(fastspeed)
-  a.ChangeDutyCycle(0)
-  b.ChangeDutyCycle(fastspeed)
-  setLEDs(0, 1, 1, 0)
-  print('straight')
-
-def turnleft():
-  p.ChangeDutyCycle(fastspeed)
-  q.ChangeDutyCycle(0)
-  a.ChangeDutyCycle(slowspeed)
-  b.ChangeDutyCycle(0)
-  setLEDs(0, 0, 1, 1)
-  print('left')
-
-def turnright():
-  p.ChangeDutyCycle(slowspeed)
-  q.ChangeDutyCycle(0)
-  a.ChangeDutyCycle(fastspeed)
-  b.ChangeDutyCycle(0)
-  setLEDs(1, 1, 0, 0)
-  print('right')
-
-def stopall():
-  p.ChangeDutyCycle(0)
-  q.ChangeDutyCycle(0)
-  a.ChangeDutyCycle(0)
-  b.ChangeDutyCycle(0)
-  setLEDs(1, 1, 1, 1)
-  print('stop')
-
-def setLEDs(L1, L2, L3, L4):
-  GPIO.output(LED1, L1)
-  GPIO.output(LED2, L2)
-  GPIO.output(LED3, L3)
-  GPIO.output(LED4, L4)
-
-setLEDs(1, 1, 1, 1) # switch all LEDs off
 
 # main loop
 try:
   while True:
-    forwards()
-    time.sleep(1)
-    reverse()
-    time.sleep(1)
-    turnright()
-    time.sleep(1)
-    turnleft()
-    time.sleep(1)
+    forward(50)
+    time.sleep(2)
+    reverse(50)
+    time.sleep(2)
+    turnright(rspeed= 20, lspeed =50)
+    time.sleep(2)
+    turnleft(rspeed = 50, lspeed = 20)
+    time.sleep(2)
     stopall()
-    time.sleep(1)
+    time.sleep(10)
 
 except KeyboardInterrupt:
-       Going = False
-       GPIO.cleanup()
-       sys.exit()
+    GPIO.cleanup()
